ab7048c96a99b855156e856ba8777f2671198a34
Zhihao Duan <duan90@purdue.edu>
2021-11-25 12:19:48 -0500

    set default device to cpu in the example

--- example.py
+++ example.py
@@ -2,7 +2,8 @@ from api import Detector
 
 # Initialize detector
 detector = Detector(model_name='rapid',
-                    weights_path='./weights/pL1_MWHB1024_Mar11_4000.ckpt')
+                    weights_path='./weights/pL1_MWHB1024_Mar11_4000.ckpt',
+                    use_cuda=False)
 
 # A simple example to run on a single image and plt.imshow() it
 detector.detect_one(img_path='./images/exhibition.jpg',
